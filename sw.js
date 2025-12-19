a0537ab5cbe06c5298bfb526dfcf90c0dfa01c58
supapa424 <lun961001024@gmail.com>
2025-12-19 20:12:33 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "health-checkin-v1";
+const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>r))
+  );
+});
