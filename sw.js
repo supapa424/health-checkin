4a66e4739422b249aa51d5b38156acf0bcc74d03
supapa424 <lun961001024@gmail.com>
2025-12-20 14:18:01 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,48 +1,17 @@
-const CACHE = "health-checkin-v7";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./sw.js",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
+const CACHE = "health-checkin-v1";
+const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", (e) => {
-  // 让新版本立刻生效（避免 iOS 长时间卡在旧缓存）
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    (async () => {
-      // 清理旧版本缓存
-      const keys = await caches.keys();
-      await Promise.all(
-        keys
-          .filter((k) => k.startsWith("health-checkin-") && k !== CACHE)
-          .map((k) => caches.delete(k))
-      );
-      // 立刻接管所有页面
-      await self.clients.claim();
-    })()
-  );
+  e.waitUntil(self.clients.claim());
 });
-
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((r) => {
-      return (
-        r ||
-        fetch(e.request)
-          .then((resp) => {
-            const copy = resp.clone();
-            caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
-            return resp;
-          })
-          .catch(() => r)
-      );
-    })
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>r))
   );
 });
